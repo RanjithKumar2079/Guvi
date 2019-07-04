75ca010234a3f62e3cce5782143010df341889d5
RanjithKumar2079 <51515394+RanjithKumar2079@users.noreply.github.com>
2019-07-04 11:52:20 +0530

    Update 3.py

--- code/3.py
+++ code/3.py
@@ -1,5 +1,5 @@
 n=input()
-if(len(n)==1 and not(n.isdigit())):
+if(len(n)==1 and not(n.isdigit()) and (n not in ["[","@","_","!","#","$","%","^","&","*","(",")","<",">","?","/","\","|","}","{","~",":","]"])):
       if(n in ["a","e","i","o","u","A","E","I","O","U"]):
               print("Vowel")
       else:
