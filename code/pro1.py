ea36ff4e1e0cbc8bde447df0ae9896868658a4e9
RanjithKumar2079 <51515394+RanjithKumar2079@users.noreply.github.com>
2019-07-04 23:42:16 +0530

    Update pro1.py

--- code/pro1.py
+++ code/pro1.py
@@ -6,9 +6,10 @@ for i in range(n):
 r=0
 k=min(j)
 for i in k:
-    if(i==j[r][r]):
-        t.append(i)
-    elif(i!=j[r][r]):
-        break
-    r=r+1
+    for g in j:
+        if(i==g[r]):
+            t.append(i)
+        elif(i!=g[r]):
+            break
+        r=r+1
 print(*t,sep="")
